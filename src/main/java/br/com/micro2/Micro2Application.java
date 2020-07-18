9ebd2a92a5764a5d0803855f931073eb07de7041
Eduardo <eduardomelle@gmail.com>
2020-07-18 01:02:04 -0300

    Correções e melhorias.

--- src/main/java/br/com/micro2/Micro2Application.java
+++ src/main/java/br/com/micro2/Micro2Application.java
@@ -7,17 +7,6 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-import com.uber.jaeger.Configuration;
-import com.uber.jaeger.samplers.ProbabilisticSampler;
-
-import brave.Tracing;
-import brave.opentracing.BraveTracer;
-import brave.sampler.Sampler;
-import zipkin.Span;
-import zipkin.reporter.AsyncReporter;
-import zipkin.reporter.Encoding;
-import zipkin.reporter.okhttp3.OkHttpSender;
-
 @SpringBootApplication
 public class Micro2Application {
 
@@ -32,21 +21,5 @@ public class Micro2Application {
 	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
 		return restTemplateBuilder.build();
 	}
-
-	// @Bean
-	public io.opentracing.Tracer jaegerTracer() {
-		return new Configuration("micro2", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
-				new Configuration.ReporterConfiguration()).getTracer();
-	}
-
-	@Bean
-	public io.opentracing.Tracer zipkinTracer() {
-		OkHttpSender okHttpSender = OkHttpSender.builder().encoding(Encoding.JSON)
-				.endpoint("http://localhost:9411/api/v1/spans").build();
-		AsyncReporter<Span> reporter = AsyncReporter.builder(okHttpSender).build();
-		Tracing braveTracer = Tracing.newBuilder().localServiceName(localServiceName).reporter(reporter)
-				.traceId128Bit(true).sampler(Sampler.ALWAYS_SAMPLE).build();
-		return BraveTracer.create(braveTracer);
-	}
-
+	
 }
--- src/main/java/br/com/micro2/resource/AddressResource.java
+++ src/main/java/br/com/micro2/resource/AddressResource.java
@@ -1,15 +1,20 @@
 package br.com.micro2.resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AddressResource {
 
+	private static final Logger LOG = LoggerFactory.getLogger(AddressResource.class);
+
 	@GetMapping("/address")
 	public String getAddress() {
-		String teste = null;
-		System.err.println(teste.trim());
+		LOG.info("ADDRESS");
+		LOG.warn("ADDRESS");
+		LOG.error("ADDRESS");
 
 		return "Rua Gurupá, 401 - Vila Anglo Brasileira - Perdizes";
 	}
